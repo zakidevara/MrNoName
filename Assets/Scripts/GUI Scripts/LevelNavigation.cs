e46d18385e842072f80ef4e18c4e494e7f2ffab3
zakidevara <mzakidevara@gmail.com>
2019-09-15 15:17:41 +0700

    Penambahan fitur level selection

---
+++ Assets/Scripts/GUI Scripts/LevelNavigation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelNavigation : MonoBehaviour
+{
+    public void level1() {
+        Application.LoadLevel("level1");
+
+    }
+}
