e08a92e6873ec0f5d43fb8b1134bf3e7b28b41b3
kresnahand <kresna0821@gmail.com>
2019-09-19 20:47:40 +0700

    penambahan script spike & map level 2

---
+++ Assets/Scripts/kill.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class kill : MonoBehaviour
+{
+    private GameManager _gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    
+    void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.name == "pacman")
+		{
+		    _gm.LoseLife();
+		}
+	}
+}
