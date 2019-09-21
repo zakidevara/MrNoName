3b22f6435a7732d40559829939392c92b0d2bafa
zakidevara <mzakidevara@gmail.com>
2019-09-21 10:58:07 +0700

    penambahan script untuk manager dan controller story mode, tetapi belum diimplementasikan

---
+++ Assets/Scripts/BabyBroMove.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class BabyBroMove : MonoBehaviour
+{
+
+    private GameManager _gm;
+
+    //-----------------------------------------------------------------------------------------
+    // variables end, functions begin
+    void Start()
+    {
+        _gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "pacman")
+        {
+
+            _gm.LoseLife();
+
+
+        }
+    }
+}
+
+
+
+
