a18b5ba0a87fb630b43527f79f541eddc38112af
zakidevara <mzakidevara@gmail.com>
2019-09-29 16:45:44 +0700

    script untuk astar, tapi masih kosong

---
+++ Assets/Scripts/AStar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType { START, GOAL, PATH, BLOCKED }
+public class AStar : MonoBehaviour
+{
+    private TileType tileType;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
