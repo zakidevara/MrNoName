8bb4c2fbcf891ebbf6810873ef2363233ec71131
kresnahand <kresna0821@gmail.com>
2019-11-17 21:27:01 +0700

    check point & lv2

---
+++ Assets/Scripts/CPlamp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlamp : MonoBehaviour
+{
+    public Light FlashLightObject;
+    private bool LightEnabled = false;
+    void Start()
+    {
+        FlashLightObject.enabled = LightEnabled;
+    }
+   
+    void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.name == "pacman" || other.name == "nosis")
+		{
+            LightEnabled = true;
+            FlashLightObject.enabled = LightEnabled;
+		}
+	}
+}
